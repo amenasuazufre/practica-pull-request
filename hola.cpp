1b9fa0765e834a4ae839107b11fdf9da889af86b
Samuel Henao <codinsamueldev@gmail.com>
2025-05-21 14:19:39 -0500

    añadir funcion para validar fondos de cuenta bancaria

--- hola.cpp
+++ hola.cpp
@@ -2,10 +2,25 @@
 
 using namespace std;
 
+
+
+void vendedor_ambulante(){
+	int cantidad;
+
+	cout << "\n\tIngresar cantidad de dinero en el banko: "; cin >> cantidad;
+	cout << "\n\tSe vende arracha, cebolla, aguacate.";
+
+	if (cantidad < 10000){
+		cout << "\n\n\tEstá pelado de villegas parce...\n";
+	}
+}
+
+
 int main(){
 	
-	cout << "quiero cafe dame cafe\n\t Tomate de aliño";
+	cout << "\n\tquiero cafe dame cafe\n\t Tomate de aliño";
 
+	vendedor_ambulante();
 	return 0;
 }
 
